af22e9a3e9c27db8ea0d3f290b916807484b6aed
JelleBoonDev <jelleboon@live.com>
2026-05-05 15:53:56 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sm-timer-v7';
+const CACHE = 'sm-timer-v8';
 
 const ASSETS = [
   './',
@@ -13,28 +13,47 @@ const ASSETS = [
 self.addEventListener('install', (e) => {
   self.skipWaiting();
   e.waitUntil(
-    caches.open(CACHE).then((c) => c.addAll(ASSETS))
+    caches.open(CACHE).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
   );
 });
 
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys()
-      .then(keys => Promise.all(
-        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
-      ))
+      .then((keys) =>
+        Promise.all(
+          keys
+            .filter((key) => key !== CACHE)
+            .map((key) => caches.delete(key))
+        )
+      )
       .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+
   e.respondWith(
     fetch(e.request)
-      .then((res) => {
-        const copy = res.clone();
-        caches.open(CACHE).then((c) => c.put(e.request, copy));
-        return res;
+      .then((response) => {
+        if (
+          response &&
+          response.status === 200 &&
+          e.request.url.startsWith(self.location.origin)
+        ) {
+          const copy = response.clone();
+          caches.open(CACHE).then((cache) => {
+            cache.put(e.request, copy);
+          });
+        }
+
+        return response;
       })
-      .catch(() => caches.match(e.request))
+      .catch(() => {
+        return caches.match(e.request, { ignoreSearch: true });
+      })
   );
 });
